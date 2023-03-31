1ae6581d190096636ff9117e184714d192816e6c
Moein-HDi <moein.hedayati@gmail.com>
2023-03-31 19:52:00 +0330

    temporarily removing map

--- pathfinder/urls.py
+++ pathfinder/urls.py
@@ -5,6 +5,6 @@ from .views import  HomePageView, StationsView, MapView
 urlpatterns = [
     
     path('', HomePageView, name='home'),
-    path('map', MapView, name='map'),
+    # path('map', MapView, name='map'),
     path('stations', StationsView, name='stations'),
 ]
