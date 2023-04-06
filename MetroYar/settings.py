c91c3f87db7c3eb43b9547f5de42358a692f5c58
Moein-HDi <moein.hedayati@gmail.com>
2023-04-06 11:17:38 +0330

    custom error pages and donation

--- MetroYar/settings.py
+++ MetroYar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cq_ks5c(hi%+af1gaj(b(&4mw5id654tvb0syg-)9us0^lf&m7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
